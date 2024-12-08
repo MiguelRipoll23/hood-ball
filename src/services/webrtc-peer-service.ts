68825ce535b2d2e39ada16f8ffb6aaaabe42d566
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2024-12-08 18:38:19 +0100

    Rename ice_servers to rtc_ice_servers in AuthenticationResponse and update related references in ServerRegistration and WebRTCPeerService

--- src/services/webrtc-peer-service.ts
+++ src/services/webrtc-peer-service.ts
@@ -44,7 +44,7 @@ export class WebRTCPeerService {
         ?.getGameState()
         ?.getGameServer()
         ?.getServerRegistration()
-        ?.getIceServers(),
+        ?.getRTCIceServers(),
     });
 
     if (this.host === false) {
