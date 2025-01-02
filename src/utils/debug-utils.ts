b7f3f7cc7d7fbd78dc6acc71e6bf2cb257083c2d
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2025-01-02 12:55:09 +0100

    Add FPS rendering and debug information to GameLoopService (#96)

--- src/utils/debug-utils.ts
+++ src/utils/debug-utils.ts
@@ -3,20 +3,24 @@ export class DebugUtils {
     context: CanvasRenderingContext2D,
     x: number,
     y: number,
-    text: string
+    text: string,
+    subtractWidth = false
   ): void {
     context.font = "12px system-ui";
 
     const textWidth = context.measureText(text).width;
     const boxWidth = textWidth + 12; // Add margin to the calculated text width
 
+    // Adjust the x-coordinate if subtractWidthWidth is true
+    const adjustedX = subtractWidth ? x - boxWidth : x;
+
     // Render background box
     context.fillStyle = "rgba(0, 0, 0, 0.6)";
-    context.fillRect(x, y, boxWidth, 20);
+    context.fillRect(adjustedX, y, boxWidth, 20);
 
     // Render text
     context.fillStyle = "#FFFF00";
     context.textAlign = "left";
-    context.fillText(text, x + 6, y + 14);
+    context.fillText(text, adjustedX + 6, y + 14);
   }
 }
