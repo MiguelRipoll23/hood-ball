5285413f7977cef202259bb98469e91541643000
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2025-01-02 23:47:15 +0100

    Refactor object ownership and inactivity handling in ObjectOrchestrator and ObjectUtils

--- src/utils/object-utils.ts
+++ src/utils/object-utils.ts
@@ -1,18 +1,7 @@
-import { GameState } from "../models/game-state.js";
-import { ObjectStateType } from "../enums/object-state-type.js";
-import { MultiplayerGameObject } from "../interfaces/object/multiplayer-game-object.js";
 import { WebRTCPeer } from "../interfaces/webrtc-peer.js";
 
 export class ObjectUtils {
-  public static handleInactiveObject(
-    multiplayerObject: MultiplayerGameObject
-  ): void {
-    if (multiplayerObject.getState() === ObjectStateType.Inactive) {
-      multiplayerObject.setRemoved(true);
-    }
-  }
-
-  public static isInvalidOwner(
+  public static hasInvalidOwner(
     webrtcPeer: WebRTCPeer,
     ownerId: string
   ): boolean {
@@ -21,19 +10,5 @@ export class ObjectUtils {
     }
 
     return webrtcPeer.getPlayer()?.getId() !== ownerId;
-  }
-
-  public static updateOwnerToHostForSharedObjects(
-    gameState: GameState,
-    multiplayerObject: MultiplayerGameObject
-  ) {
-    const owner = multiplayerObject.getOwner();
-
-    if (owner !== null) {
-      return;
-    }
-
-    const host = gameState.getMatch()?.getHost() ?? null;
-    multiplayerObject.setOwner(host);
   }
 }
