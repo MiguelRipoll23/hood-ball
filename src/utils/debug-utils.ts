70f5d328e2dcb484d35f4eef43154fc924b1a778
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2025-01-04 18:51:41 +0100

    Increase font size and adjust background box dimensions in debug utilities for better visibility (#137)

--- src/utils/debug-utils.ts
+++ src/utils/debug-utils.ts
@@ -7,22 +7,22 @@ export class DebugUtils {
     subtractWidth = false,
     subtractHeight = false
   ): void {
-    context.font = "12px system-ui";
+    context.font = "14px system-ui";
 
     const textWidth = context.measureText(text).width;
     const boxWidth = textWidth + 12; // Add margin to the calculated text width
 
     // Adjust the x-coordinate if subtractWidthWidth is true
     const adjustedX = subtractWidth ? x - boxWidth : x;
-    const adjustedY = subtractHeight ? y - 20 : y;
+    const adjustedY = subtractHeight ? y - 22 : y;
 
     // Render background box
     context.fillStyle = "rgba(0, 0, 0, 0.6)";
-    context.fillRect(adjustedX, adjustedY, boxWidth, 20);
+    context.fillRect(adjustedX, adjustedY, boxWidth, 22);
 
     // Render text
     context.fillStyle = "#FFFF00";
     context.textAlign = "left";
-    context.fillText(text, adjustedX + 6, adjustedY + 14);
+    context.fillText(text, adjustedX + 6, adjustedY + 16);
   }
 }
