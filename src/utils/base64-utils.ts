d82d14b49596ba845b4e7f5c8a925ad70455ce81
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2025-05-27 15:15:13 +0200

    Refactor arrayBufferToBase64Url method for improved readability

--- src/utils/base64-utils.ts
+++ src/utils/base64-utils.ts
@@ -2,7 +2,14 @@ export class Base64Utils {
   public static arrayBufferToBase64Url(
     arrayBuffer: ArrayBuffer | ArrayBufferLike
   ): string {
-    const base64 = btoa(String.fromCharCode(...new Uint8Array(arrayBuffer)));
+    const bytes = new Uint8Array(arrayBuffer);
+
+    let binary = "";
+
+    for (let i = 0; i < bytes.length; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+    const base64 = btoa(binary);
 
     return base64.replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, "");
   }
@@ -10,8 +17,10 @@ export class Base64Utils {
   public static base64UrlToArrayBuffer(base64url: string): ArrayBuffer {
     base64url = base64url.replace(/-/g, "+").replace(/_/g, "/");
     base64url += "=".repeat((4 - (base64url.length % 4)) % 4);
+
     const binary = atob(base64url);
     const bytes = new Uint8Array(binary.length);
+
     for (let i = 0; i < binary.length; i++) {
       bytes[i] = binary.charCodeAt(i);
     }
