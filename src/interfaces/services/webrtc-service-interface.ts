6e7350818e07b46761e8b1e19382e0459fe43383
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2025-06-29 14:59:00 +0200

    Refactor registerCommandHandlers parameter type from 'any' to 'object'

--- src/interfaces/services/webrtc-service-interface.ts
+++ src/interfaces/services/webrtc-service-interface.ts
@@ -2,7 +2,7 @@ import type { WebRTCPeer } from "../webrtc-peer.js";
 import type { BinaryReader } from "../../utils/binary-reader-utils.js";
 
 export interface IWebRTCService {
-  registerCommandHandlers(instance: any): void;
+  registerCommandHandlers(instance: object): void;
   dispatchCommand(
     commandId: number,
     peer: WebRTCPeer,
