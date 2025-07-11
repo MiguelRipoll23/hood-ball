a77f19ace73586ceb107b68e0a0cc19202fdc824
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2025-07-11 16:59:43 +0200

    Revert changes for collisions (#439)

--- src/core/scenes/base-colliding-game-scene.ts
+++ src/core/scenes/base-colliding-game-scene.ts
@@ -208,8 +208,8 @@ export class BaseCollidingGameScene extends BaseMultiplayerScene {
 
     // Calculate relative velocity
     const vRelativeVelocity = {
-      x: dynamicCollidingEntity.getVX() - otherDynamicCollidingEntity.getVX(),
-      y: dynamicCollidingEntity.getVY() - otherDynamicCollidingEntity.getVY(),
+      x: otherDynamicCollidingEntity.getVX() - dynamicCollidingEntity.getVX(),
+      y: otherDynamicCollidingEntity.getVY() - dynamicCollidingEntity.getVY(),
     };
 
     // Calculate speed along collision normal
@@ -217,7 +217,7 @@ export class BaseCollidingGameScene extends BaseMultiplayerScene {
       vRelativeVelocity.x * vCollisionNorm.x +
       vRelativeVelocity.y * vCollisionNorm.y;
 
-    if (speed <= 0) {
+    if (speed < 0) {
       // Collision has already been resolved
       return;
     }
@@ -237,14 +237,15 @@ export class BaseCollidingGameScene extends BaseMultiplayerScene {
       impulse * otherDynamicCollidingEntity.getMass() * vCollisionNorm.x;
     const impulseY =
       impulse * otherDynamicCollidingEntity.getMass() * vCollisionNorm.y;
-    dynamicCollidingEntity.setVX(dynamicCollidingEntity.getVX() - impulseX);
-    dynamicCollidingEntity.setVY(dynamicCollidingEntity.getVY() - impulseY);
+
+    dynamicCollidingEntity.setVX(dynamicCollidingEntity.getVX() + impulseX);
+    dynamicCollidingEntity.setVY(dynamicCollidingEntity.getVY() + impulseY);
 
     otherDynamicCollidingEntity.setVX(
-      otherDynamicCollidingEntity.getVX() + impulseX
+      otherDynamicCollidingEntity.getVX() - impulseX
     );
     otherDynamicCollidingEntity.setVY(
-      otherDynamicCollidingEntity.getVY() + impulseY
+      otherDynamicCollidingEntity.getVY() - impulseY
     );
   }
 }
