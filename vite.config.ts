3c0f8edcb6bb72528b76244c5f62221713216b47
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2025-08-09 19:28:05 +0200

    Update base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/hood-ball/",
+  base: "/",
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
