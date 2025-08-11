8a4875e4c8f9e83ce1c2afb4071499c4ca774c94
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2025-08-12 00:23:01 +0200

    Update GAME_VERSION to 3.0.0 for the latest release

--- src/game/constants/game-constants.ts
+++ src/game/constants/game-constants.ts
@@ -1,4 +1,4 @@
-export const GAME_VERSION = "2.0.0";
+export const GAME_VERSION = "3.0.0";
 
 // Horizontal distance in pixels used when spawning cars. Cars are distributed
 // around the center of the screen using this value as spacing so they don't
