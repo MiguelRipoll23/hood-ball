14cc2c0283e547168018aad97d1c81da470ef790
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2025-08-13 23:49:43 +0200

    Add launch configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
+    open: "http://localhost:5173",
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
       "Cross-Origin-Opener-Policy": "same-origin",
